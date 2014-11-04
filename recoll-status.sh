1949bc74639af6e30e100c49448266bc25afea51
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-11-04 15:01:32 -0600

    Todos.

--- recoll-status.sh
+++ recoll-status.sh
@@ -6,3 +6,8 @@ then
 else
 	echo 'recollindex is not running.'
 fi
+
+# TODO: recollindex has been running for...
+# TODO: recollindex has indexed #/# files (#%)
+# TODO: check that recoll is installed and there is a ~/.recoll folder.
+# TODO: check when it was last opened.
