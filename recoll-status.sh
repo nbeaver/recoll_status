5ca6c5b4d4a12aaf62a663ff629b4aab11d03392
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-11-05 00:41:26 -0600

    Factor statuses into separate functions.

--- recoll-status.sh
+++ recoll-status.sh
@@ -11,27 +11,35 @@ function print_duration {
 	local hours=$(($total_seconds / $s_per_h))
 	local minutes=$(($total_seconds / $s_per_m))
 	local seconds=$(($total_seconds - $s_per_m*$minutes - $s_per_h*$hours - $s_per_d*$days))
-	printf "$days days $hours hours $minutes minutes"
+	printf "$days days $hours hours $minutes minutes\n"
+}
+
+function status_running {
+	echo 'recollindex is running.'
+	local date_start=$(date +%c --reference ~/.recoll/xapiandb/flintlock)
+	local secs_start=$(date +%s --reference ~/.recoll/xapiandb/flintlock)
+	local secs_now=$(date +%s)
+	local secs_since_start=$(($secs_now - $secs_start))
+	printf 'recollindex has been running for '
+	print_duration "$secs_since_start"
+}
+
+function status_not_running {
+	echo 'recollindex is not running.'
+	local date_last_index=$(date +%c --reference ~/.recoll/idxstatus.txt)
+	local secs_last_index=$(date +%s --reference ~/.recoll/idxstatus.txt)
+	local secs_now=$(date +%s)
+	local secs_since_last_index=$(($secs_now - $secs_last_index))
+	echo "recollindex was last run on $date_last_index"
+	printf "duration since last run: "
+	print_duration "$secs_since_last_index"
 }
 
 if [ -s ~/.recoll/index.pid ]
 then
-	echo 'recollindex is running.'
-	date_start=$(date +%c --reference ~/.recoll/xapiandb/flintlock)
-	secs_start=$(date +%s --reference ~/.recoll/xapiandb/flintlock)
-	secs_now=$(date +%s)
-	secs_since_start=$(($secs_now - $secs_start))
-	printf 'recollindex has been running for '
-	print_duration "$secs_since_start"
+	status_running
 else
-	echo 'recollindex is not running.'
-	date_last_index=$(date +%c --reference ~/.recoll/idxstatus.txt)
-	secs_last_index=$(date +%s --reference ~/.recoll/idxstatus.txt)
-	secs_now=$(date +%s)
-	secs_since_last_index=$(($secs_now - $secs_last_index))
-	echo "recollindex was last run on $date_last_index"
-	printf "duration since last run: "
-	print_duration "$secs_since_last_index"
+	status_not_running
 fi
 
 # TODO: recollindex has been running for # minutes.
