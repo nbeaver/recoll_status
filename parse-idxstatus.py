2e849614ccad433e43842fd54bc4918b4fbfa642
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-09-13 17:34:59 -0500

    Make this work as a module, too.

--- parse-idxstatus.py
+++ parse-idxstatus.py
@@ -13,29 +13,31 @@ DblxStatus = {
 '6' : "DBIXS_DONE",
 }
 
-if len(sys.argv) == 2:
-    idxstatus_path = sys.argv[1]
-else:
-    sys.stderr.write("Usage: {} /path/to/idx-status.txt\n".format(sys.argv[0]))
-    sys.exit(1)
+def check_idxstatus(idxstatus_path):
+    with open(idxstatus_path) as db_status:
+        for line in db_status:
+            key, val = (x.strip() for x in line.split('=', 1))
+            if key == 'phase':
+                print('DblxStatus is',DblxStatus[val])
+                status = val
+            elif key == 'docsdone':
+                print('Files indexed:',val)
+                files_indexed = int(val)
+            elif key == 'filesdone':
+                print('Files checked:',val)
+                files_checked = int(val)
+            elif key == 'dbtotdocs':
+                print('Starting number of files:',val)
+                num_initial_files = int(val)
+            elif key == 'fn':
+                if status == '1':
+                    print('Indexing this file or directory:', val)
+                else:
+                    print('Not indexing files now.')
 
-with open(idxstatus_path) as db_status:
-    for line in db_status:
-        key, val = (x.strip() for x in line.split('=', 1))
-        if key == 'phase':
-            print('DblxStatus is',DblxStatus[val])
-            status = val
-        elif key == 'docsdone':
-            print('Files indexed:',val)
-            files_indexed = int(val)
-        elif key == 'filesdone':
-            print('Files checked:',val)
-            files_checked = int(val)
-        elif key == 'dbtotdocs':
-            print('Starting number of files:',val)
-            num_initial_files = int(val)
-        elif key == 'fn':
-            if status == '1':
-                print('Indexing this file or directory:', val)
-            else:
-                print('Not indexing files now.')
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        idxstatus_path = sys.argv[1]
+    else:
+        sys.stderr.write("Usage: {} /path/to/idx-status.txt\n".format(sys.argv[0]))
+        sys.exit(1)
