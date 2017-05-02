697491001e3f15780c5cc364f5a46a2c3b405aac
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2017-05-02 16:12:12 -0500

    Give name of phase.

--- recollstatus.py
+++ recollstatus.py
@@ -102,10 +102,9 @@ def format_idxstatus(idxstatus):
         '5' : "DBIXS_MONITOR",
         '6' : "DBIXS_DONE",
     }
-    formatted = []
+    formatted = ['DbIxStatus is {}: {}'.format(idxstatus['phase'], DbIxStatus[idxstatus['phase']])]
     # https://bitbucket.org/medoc/recoll/src/dabc5bae1dd7f8b5049ef021c441ffb8050cd7eb/src/index/indexer.h?at=default&fileviewer=file-view-default#indexer.h-40
     descriptors = collections.OrderedDict()
-    descriptors['phase'] = 'DbIxStatus is'
     descriptors['docsdone'] = 'Documents updated:'
     descriptors['filesdone'] = 'Files tested:'
     descriptors['filerrors'] = 'Failed files:'
